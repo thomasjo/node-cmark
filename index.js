6fe0ef1c7088f9951700962967f7f0509a6ae267
Thomas Johansen <thomasjo@gmail.com>
2017-10-08 14:15:39 +0200

    Remove hacky test stuff from main module

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-const cmark = require("bindings")("cmark.node")
-
+const cmark = require('bindings')('cmark.node')
 module.exports = cmark
-
-// NOTE: Silly stuff to make manual testing more convenient during early development...
-if (process.argv.length > 2) {
-  const args = process.argv.slice(2)
-  console.log(cmark.toHtml(args[0]))
-}
