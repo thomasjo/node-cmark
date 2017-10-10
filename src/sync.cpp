f4670fc476d128da77dde247b83613bfdedad5a8
Thomas Johansen <thomasjo@gmail.com>
2017-10-10 13:00:14 +0200

    Ensure the markdown argument is a string

--- src/sync.cpp
+++ src/sync.cpp
@@ -9,6 +9,11 @@
 
 NAN_METHOD(cmark::render_html_sync)
 {
+  if (!info[0]->IsString()) {
+    Nan::ThrowTypeError("Expected argument 1 to be of type String.");
+    return;
+  }
+
   const auto markdown = *Nan::Utf8String(info[0]->ToString());
   const auto html = cmark::markdown_to_html(markdown, 0);
 
